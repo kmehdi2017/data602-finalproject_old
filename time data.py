7cde5bc6822567140c9f938eefc988ea72ae933b
dquarshie89 <dquarshie89@gmail.com>
2018-05-15 09:55:51 -0400

    Update time data.py

--- time data.py
+++ time data.py
@@ -28,11 +28,13 @@ def predict():
         full_list = full_list.append(full_market_info, ignore_index=True)
 
     for n in currencies:
-        pre_market_info = pd.read_html("https://coinmarketcap.com/currencies/"+n+"/historical-data/?start=201501010&end="+spike_date)[0]
-        #pre_market_info.loc[pre_market_info['Volume']=="-",'Volume']=0
-        pre_market_info['Volume'] = pre_market_info['Volume'].astype('int64')
-        pre_market_info["Coin"] = n
-        pre_spike = pre_spike.append(pre_market_info, ignore_index=True)
+        pre_market_info = pd.read_html("https://coinmarketcap.com/currencies/"+n+"/historical-data/?start=20150101&end="+spike_date)[0]
+        if (pre_market_info[['Date']]=='No data was found for the selected time period.')== True:
+            pass
+        else:
+            pre_market_info['Volume'] = pre_market_info['Volume'].astype('int64')
+            pre_market_info["Coin"] = n
+            pre_spike = pre_spike.append(pre_market_info, ignore_index=True)
 
     for o in currencies:
         post_market_info = pd.read_html("https://coinmarketcap.com/currencies/"+o+"/historical-data/?start="+spike_date+"&end="+time.strftime("%Y%m%d"))[0]
